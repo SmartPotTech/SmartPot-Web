0ca2e9a9397ac2add48d3ff22fccb24e6acc5ae6
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2024-10-21 13:51:17 -0500

    structure change

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from '/assets/images/react.svg'
+import reactLogo from '../assets/images/react.svg'
 import viteLogo from '/vite.svg'
-import '/assets/styles/app.css'
+import '../assets/styles/app.css'
 
 function App() {
   const [count, setCount] = useState(0)
