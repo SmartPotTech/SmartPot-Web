e6d3a9778933e2193cceb6bc297675b238612654
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2024-11-10 20:45:02 -0500

    tailwindcss postcss autoprefixer is installed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Esta línea incluye tus archivos TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
