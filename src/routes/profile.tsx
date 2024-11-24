1ee64af9a91838e0fd4c7e274f8c55862afc637f
andres2204 <medinaandres2204@gmail.com>
2024-11-23 19:50:33 -0500

    Update user function added to authContext

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -2,12 +2,30 @@ import { useAuthContext } from "../contexts/AuthContext"
 
 export default function Profile() {
 
-    const {user} = useAuthContext();
+    const {user, updateUser } = useAuthContext();
+
+    const handleUpdate = () => {
+        if (user != null) {
+            updateUser({
+                name: "Pedro",
+                lastname: "$",
+                email: null,
+                password: null,
+                role: null
+            })
+        }
+    }
 
     return (
         <>
             This is the profile page! 
-            Welcome {user?.name}
+            Welcome {JSON.stringify(user)}
+
+            Want to update? click this button!
+            <button 
+                className="btn btn-primary"
+                onClick={handleUpdate}
+            >update!</button>
         </>
     )
 
