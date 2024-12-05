73e83d5a34b5680103cac3de38b85607b0bce594
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2024-12-05 15:39:36 -0500

    Migrate from D3 to Plotly charts, improve typing, and update layout
    Migrated chart implementation from D3 to Plotly for improved user experience and code simplicity. Installed TypeScript types for `react-plotly.js` and resolved TS7016 by adding a manual declaration file for the module. Updated the layout to use a single column (`grid-cols-1`) in Tailwind CSS, ensuring charts are displayed in one column regardless of screen size. Enhanced design to make charts occupy 100% of the available height, improving visibility and spacing. Refactored the `PlotlyChart` component to dynamically handle different measures and labels, resulting in cleaner and more modular code.

--- src/components/SideBarFactory/SiderbarTablet.tsx
+++ src/components/SideBarFactory/SiderbarTablet.tsx
@@ -1,5 +1,5 @@
 // SidebarTablet.tsx
-import React from "react";
+//import React from "react";
 import { SidebarProps, SidebarInterface } from "./ISideBar.ts";
 import SidebarBase from "./SidebarBase";
 
