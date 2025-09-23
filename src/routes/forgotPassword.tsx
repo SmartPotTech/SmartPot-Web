865b1e90b17f4579dd3126d293df49212f453650
DarwinC <dc162005@gmail.com>
2025-09-23 05:56:49 -0500

    Add forgot password page and reset password component
    - Introduced `forgotPassword.tsx` as a dedicated page for password recovery.
    - Created `ResetPassword.tsx` component to handle password reset input and submission.
    - Linked `login.tsx` to`forgotPassword.tsx to ensure smooth navigation after reset.

--- src/routes/forgotPassword.tsx
+++ src/routes/forgotPassword.tsx
@@ -4,7 +4,7 @@ import {useAuthContext} from "../contexts/AuthContext.tsx";
 import {useNavigate} from "react-router-dom";
 import Loading from "../components/Loading.tsx"
 
-export default function Register() {
+export default function ForgotPassword() {
     const {register, isAuthenticated, error, loading} = useAuthContext();
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
@@ -42,7 +42,7 @@ export default function Register() {
     return (
         <>
             {}
-            <div id="loginCard" className="bg-white rounded-2xl p-8 shadow flex min-h-full flex-1 flex-col justify-center px-6 sm:px-10 lg:px-16 ">
+            <div id="loginCard" className="bg-white rounded-2xl p-8 shadow flex min-h-full flex-1 flex-col justify-center px-6 sm:px-10 lg:px-16">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img
                         alt="smartpot"
@@ -50,7 +50,7 @@ export default function Register() {
                         className="mx-auto h-10 w-auto"
                     />
                     <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                        Create your account
+                        Reset Password
                     </h2>
                 </div>
 
@@ -61,44 +61,6 @@ export default function Register() {
                         className="space-y-6"
                         onSubmit={handleSubmit}
                     >
-                        <div>
-                            <label htmlFor="name"
-                                   className="block text-sm/6 font-medium text-gray-900 flex items-center justify-between">
-                                Name
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    id="name"
-                                    name="name"
-                                    type="text"
-                                    required
-                                    autoComplete="given-name"
-                                    className="bg-white font-semibold p-2 block w-full rounded-md border-0 py-1.5 text-gray-800 shadow-sm ring-1 ring-inset ring-green-300 placeholder:text-green-400 focus:ring-2 focus:ring-inset focus:ring-green-300 sm:text-sm/6"
-                                    value={formData.name}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <label htmlFor="lastname"
-                                   className="block text-sm/6 font-medium text-gray-900 flex items-center justify-between">
-                                Last Name
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    id="lastname"
-                                    name="lastname"
-                                    type="text"
-                                    required
-                                    autoComplete="family-name"
-                                    className="bg-white font-semibold p-2 block w-full rounded-md border-0 py-1.5 text-gray-800 shadow-sm ring-1 ring-inset ring-green-300 placeholder:text-green-400 focus:ring-2 focus:ring-inset focus:ring-green-300 sm:text-sm/6"
-                                    value={formData.lastname}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                        </div>
-
                         <div>
                             <label htmlFor="email"
                                    className="block text-sm/6 font-medium text-gray-900 flex items-center justify-between">
@@ -118,25 +80,7 @@ export default function Register() {
                             </div>
                         </div>
 
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-                                    Password
-                                </label>
-                            </div>
-                            <div className="mt-2">
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    required
-                                    autoComplete="new-password"
-                                    className="bg-white font-semibold p-2 block w-full rounded-md border-0 py-1.5 text-gray-800 shadow-sm ring-1 ring-inset ring-green-300 placeholder:text-green-400 focus:ring-2 focus:ring-inset focus:ring-green-300 sm:text-sm/6"
-                                    value={formData.password}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                        </div>
+
                         <div>
                             <p className="mt-10 text-center text-sm/6 text-red-500">
                                 {error}
@@ -147,15 +91,15 @@ export default function Register() {
                                 type="submit"
                                 className="bg-main-colour flex w-full justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                             >
-                                {loading ? ("Loading") : ("Sign up")}
+                                {loading ? ("Loading") : ("Reset Password")}
                             </button>
                         </div>
                     </form>
 
                     <p className="mt-10 text-center text-sm/6 text-gray-500">
-                        Do you have an account?{" "}
-                        <a href="/auth/login" className="font-semibold text-green-600 hover:text-green-500">
-                            Sign in to your account
+                        Don’t have an account yet?{" "}
+                        <a href="/auth/register" className="font-semibold text-green-600 hover:text-green-500">
+                            Create your account
                         </a>
                     </p>
                 </div>
