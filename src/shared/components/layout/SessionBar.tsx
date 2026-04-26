19bdf42dc82210d4522cd7087263b0459a5f5540
Sebastián López O <sebastianlopezosorno2005@gmail.com>
2026-04-26 09:31:22 -0500

    Update SessionBar.tsx

--- src/shared/components/layout/SessionBar.tsx
+++ src/shared/components/layout/SessionBar.tsx
@@ -1,10 +1,10 @@
-import { UserOutlined } from "@ant-design/icons";
+import {UserOutlined} from "@ant-design/icons";
 import NotificationBell from "../../../features/notifications/components/NotificationBell.tsx";
-import { useAuthContext } from "../../../features/auth/contexts/AuthContext.tsx";
+import {useAuthContext} from "../../../features/auth/contexts/AuthContext.tsx";
 
 export default function SessionBar() {
 
-    const { user } = useAuthContext();
+    const {user} = useAuthContext();
 
     return (
         <header className="z-20 top-0 right-0 fixed flex items-center justify-end h-16 px-4
@@ -13,17 +13,18 @@ export default function SessionBar() {
             min-[1025px]:left-[15rem]
             bg-[#F3F2F7]">
 
-            <NotificationBell />
+            <NotificationBell/>
 
             {/* Divider vertical */}
-            <div className="h-[30px] border-l-[3px] border-[#cfcdd6] ml-[30px] mr-2.5" />
+            <div className="h-[30px] border-l-[3px] border-[#cfcdd6] ml-[30px] mr-2.5"/>
 
             <span className="font-['Helvetica',serif] inline ml-2.5 mr-2.5 text-black font-semibold">
                 👋 ¡Hola {user?.name}!
             </span>
 
             <a href="/perfil">
-                <UserOutlined className="inline ml-2.5 mr-2.5 pr-[15px] text-[27px] text-[#00B074] rounded-full align-middle" />
+                <UserOutlined
+                    className="inline ml-2.5 mr-2.5 pr-[15px] text-[27px] text-[#00B074] rounded-full align-middle"/>
             </a>
         </header>
     )
